d707c7c0badd5444bd61026b6cca2cc38cbd4bfc
Pete Lewis <me@pjblewis.com>
2023-02-16 13:49:02 -0800

    Fixes

--- lnwin.cpp
+++ lnwin.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <filesystem>
 
 void printUsage()
 {
-	std::cout << "Usage: lnwin [source] [destination]" << std::endl;
+	std::cout << "Usage:" << std::endl;
+	std::cout << "  lnwin -s [-f] <source> <destination>" << std::endl;
+	std::cout << "  lnwin -?" << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-	if (argc != 4) {
+	if (argc < 4) {
 		printUsage();
-		return 1;
+		return EXIT_FAILURE;
 	}
 
 	const char* switches = argv[1];
@@ -19,7 +22,7 @@ int main(int argc, char** argv)
 	bool force = false;
 	if (switches[0] != '-') {
 		printUsage();
-		return 1;
+		return EXIT_FAILURE;
 	}
 	for (int i = 1; switches[i] != '\0'; i++) {
 		switch (switches[i]) {
@@ -32,15 +35,15 @@ int main(int argc, char** argv)
 		case 'h':
 		case '?':
 			printUsage();
-			return 0;
+			return EXIT_SUCCESS;
 		default:
 			printUsage();
-			return 1;
+			return EXIT_FAILURE;
 		}
 	}
 
 	if (!createSymbolic) {
-		std::cout << "Only symbolic links are supported at this time." << std::endl;
+		std::cerr << "Only symbolic links are supported at this time." << std::endl;
 		return 1;
 	}
 
@@ -49,23 +52,32 @@ int main(int argc, char** argv)
 
 	// Validate the target and linkname parameters
 	if (target == nullptr || linkName == nullptr) {
-		std::cout << "Invalid target or linkname" << std::endl;
+		std::cerr << "Invalid target or linkname" << std::endl;
 		return 1;
 	}
 
 	// Check whether linkName already exists on disk
-	if (GetFileAttributesA(linkName) != INVALID_FILE_ATTRIBUTES) {
+	auto attributes = GetFileAttributesA(linkName);
+	if (attributes != INVALID_FILE_ATTRIBUTES) {
 		if (!force) {
-			std::cout << "Link already exists. Use -f to force overwrite." << std::endl;
-			return 1;
+			std::cerr << linkName << " already exists. Use -f to force overwrite." << std::endl;
+			return EXIT_FAILURE;
+		}
+		else if ((attributes & FILE_ATTRIBUTE_REPARSE_POINT) == 0) {
+			std::cerr << "Cowardly refusing to overwrite existing non-symlink: " << linkName << std::endl;
+			return EXIT_FAILURE;
+		}
+		else if (!std::filesystem::remove(linkName)) {
+			std::cerr << "Failed to remove existing link: " << linkName << " (" << GetLastError() << ")" << std::endl;
+			return EXIT_FAILURE;
 		}
 	}
 
 	// Determine whether the source is a file or a directory
-	DWORD attributes = GetFileAttributesA(target);
-	if (attributes == INVALID_FILE_ATTRIBUTES) {
-		std::cout << "Error: Unable to determine the type of the source file." << std::endl;
-		return 1;
+	attributes = GetFileAttributesA(target);
+	if (!force && attributes == INVALID_FILE_ATTRIBUTES) {
+		std::cerr << "Error: Unable to determine the type of the source file." << std::endl;
+		return EXIT_FAILURE;
 	}
 
 	// If it's a directory, add the directory flag
@@ -80,11 +92,11 @@ int main(int argc, char** argv)
 		size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
 			NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
 		
-		std::cout << "Failed to create hard link to " << target << " at " << linkName << ": " << messageBuffer << std::endl;
+		std::cerr << "Failed to create link to " << target << " at " << linkName << ": " << messageBuffer << std::endl;
 
 		LocalFree(messageBuffer);
-		return 1;
+		return EXIT_FAILURE;
 	}
 	
-	return 0;
+	return EXIT_SUCCESS;
 }
